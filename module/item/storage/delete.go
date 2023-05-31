0d4f8a972b1a02c3824e7acc3506d813b62e3c48
Hoang Tran <hoangtk.0100@gmail.com>
2023-06-01 00:05:10 +0700

    Implement delete todo API

---
+++ module/item/storage/delete.go
@@ -0,0 +1,21 @@
+package storage
+
+import (
+	"context"
+
+	"github.com/hoangtk0100/social-todo-list/module/item/model"
+)
+
+func (store *sqlStore) DeleteItem(ctx context.Context, cond map[string]interface{}) error {
+	deletedStatus := "Deleted"
+
+	if err := store.db.Table(model.TodoItem{}.TableName()).
+		Where(cond).
+		Updates(map[string]interface{}{
+			"status": deletedStatus,
+		}).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ module/item/transport/gin/delete_item_by_id_handler.go
@@ -0,0 +1,39 @@
+package ginitem
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hoangtk0100/social-todo-list/common"
+	"github.com/hoangtk0100/social-todo-list/module/item/biz"
+	"github.com/hoangtk0100/social-todo-list/module/item/storage"
+	"gorm.io/gorm"
+)
+
+func DeleteItem(db *gorm.DB) func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		store := storage.NewSQLStore(db)
+		business := biz.NewDeleteItemBiz(store)
+
+		if err := business.DeleteItemById(c.Request.Context(), id); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(true))
+	}
+}
